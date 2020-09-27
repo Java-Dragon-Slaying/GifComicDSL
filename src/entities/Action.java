eeb7bde77db0a2e18219ef0df464283663ed5ed8
Protouch <protouch.tiles@gmail.com>
2020-09-27 16:18:08 -0700

    added entities, some folders

---
+++ src/entities/Action.java
@@ -0,0 +1,8 @@
+package entities;
+
+import java.util.List;
+
+public class Action {
+    List<Image> imageList;
+
+}
---
+++ src/entities/GifComicProgram.java
@@ -0,0 +1,9 @@
+package entities;
+
+import java.util.List;
+
+public class GifComicProgram {
+    String name;
+    List<Image> imageList;
+    List<Panel> panelList;
+}
