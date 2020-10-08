32e8607880b4adf79f7d113595dc1afa6ee73ffa
g0u1b <g0u1b@ugrad.cs.ubc.ca>
2020-10-08 16:11:46 -0700

    change image enum to hashmap

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,8 @@
-import entities.GifComicProgram;
+import ast.CreateImage;
 import ast.Statement;
+import entities.GifComicProgram;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 public class Parser {
@@ -24,12 +26,26 @@ public class Parser {
     }
 
     private Statement parseStatement() {
-        if (tokenizer.checkToken())
+        if (tokenizer.checkToken("use")) {
+            return parseCreateImage();
+        } else if (tokenizer.checkToken("create")) {
+            return parseCreatePanel();
+        } else {
+            throw new InvalidParameterException("PARSER: unknown statement with token " + tokenizer.getNext());
+        }
     }
 
 
-//    ITEM ::= (“use” IMAGE | "create" PANEL) (";")?
+//    ITEM ::= (“use” IMAGE | "create" PANEL) (";")? - semicolon not kept as token
+
 //    IMAGE ::= “image” SOURCE “as” NAME
+    private CreateImage parseCreateImage() {
+        tokenizer.getAndCheckNext("image");
+        String source = tokenizer.getAndCheckNext(NAME);
+        tokenizer.getAndCheckNext("as");
+        String name = tokenizer.getAndCheckNext(NAME);
+        return new CreateImage(source, name);
+    }
 //    SOURCE ::= “tall_guy” | “basketball” | <other elements of enum>
 //    PANEL::= “panel” POSITION “background” NAME (TEXT)? ADD (ADD | MOVE | REMOVE)*
 //    TEXT::= “text” (“fontsize” NUM)? """ (\w)+ """
